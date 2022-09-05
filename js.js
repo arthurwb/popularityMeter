91145c48a7e6cf14091ea509fcb22a2a15583d08
arthurwb <arthurwb@mail.uc.edu>
2022-09-05 12:53:39 -0400

    add a wait for the json response

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
-const jsonPath = '/popularityMeter/data.json';
-const response = fetch(jsonPath);
-const data = response.json();
+const jsonPath = './data.json';
+fetch(jsonPath).then(response => {
+    const data = response.json();
+});
 
 function getData() {
     console.log(data);
