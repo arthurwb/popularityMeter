a17693daf2b5a919f35862c96a1851fbf6bf1a8a
Brooks <89482331+arthurwb@users.noreply.github.com>
2022-09-04 21:54:06 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+const jsonPath = '/data.json';
+async function getData() {
+    const response = await fetch(jsonPath);
+    const data = await response.json();
+    console.log(data);
+}
+
+function init() {
+    getData();
+}
+window.onload = init();
